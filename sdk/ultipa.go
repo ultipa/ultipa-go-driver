0dab7ad67ab1c4ffbb203fc8312bcda7ec0dee84
yujincheng <272167037@qq.com>
2025-06-04 12:21:32 +0800

    modify version

--- sdk/ultipa.go
+++ sdk/ultipa.go
@@ -8,7 +8,7 @@ import (
 )
 
 // Version represents the current version of the SDK.
-const Version = "5.0.1-s5.0"
+const Version = "5.1.1-s5.1"
 
 // NewUltipaDriver Create an Ultipa Client
 func NewUltipaDriver(config *configuration.UltipaConfig) (*api.UltipaAPI, error) {
